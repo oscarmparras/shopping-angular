4a56642b35372611ffb0ab5ab2ad0e337f06ee97
Jeremy D. Young <jeremydyoung77@gmail.com>
2014-08-16 23:33:47 -0500

    Really Basic consumer of Items

---
+++ js/shopping.js
@@ -0,0 +1,10 @@
+//(function() {
+var app = angular.module('shopApp', []);
+app.controller('ShopController', ['$http', function($http) {
+  var shop = this;
+  shop.items = [];
+  $http.get('http://localhost:8080/shopping-grails/items').success(function(data) {
+    shop.items = data;
+  });
+}]);
+//})();
