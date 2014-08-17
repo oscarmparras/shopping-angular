ed7090caff87d321dc8fce58c97f079af1b869cf
Jeremy D. Young <jeremydyoung77@gmail.com>
2014-08-17 17:59:49 -0500

    Improvements to angular list page

--- js/shopping.js
+++ js/shopping.js
@@ -1,10 +1,10 @@
-//(function() {
-var app = angular.module('shopApp', []);
-app.controller('ShopController', ['$http', function($http) {
-  var shop = this;
-  shop.items = [];
-  $http.get('http://localhost:8080/shopping-grails/items').success(function(data) {
-    shop.items = data;
-  });
-}]);
-//})();
+(function() {
+	var app = angular.module('shopApp', []);
+	app.controller('ShopController', ['$http', function($http) {
+		var shop = this;
+		shop.items = [];
+		$http.get('http://localhost:8080/shopping-grails/items').success(function(data) {
+			shop.items = data;
+		});
+	}]);
+})();
